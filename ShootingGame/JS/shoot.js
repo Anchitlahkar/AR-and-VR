d7bcd8da09a6d3cfdf85a322f7a02d3afe64e1f5
Anchit Lahkar <anchitlahkar0202@gmail.com>
2021-11-27 12:43:48 +0530

    27.11.2021

--- ShootingGame/JS/shoot.js
+++ ShootingGame/JS/shoot.js
@@ -28,18 +28,69 @@ AFRAME.registerComponent("bullets", {
           z: pos.z,
         });
 
+        bullet.setAttribute("dynamic-body", {
+          shape: "sphere",
+          mass: "0",
+        });
+
         var camera = document.querySelector("#camera").object3D;
 
         //get the camera direction as Three.js vector
-        var direction = new THREE.Vector3()
-        camera.getWorldDirection(direction)
+        var direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
 
-        bullet.setAttribute("velocity",direction.multiplyScalar(-10))
+        bullet.setAttribute("velocity", direction.multiplyScalar(-10));
 
-        var scene = document.querySelector("#scene")
+    
+
+        var scene = document.querySelector("#scene");
+
+        bullet.addEventListener("collide", this.removeBullet);
 
         scene.appendChild(bullet);
       }
     });
+  },
+
+  removeBullet: function (e) {
+    //e.detail.target.el
+    //Original entity(bullet)
+    console.log(e.detail.target.el);
+    //Other entity which bullet touched
+    console.log(e.detail.body.el);
+
+    //bullet element
+    var element = e.detail.target.el;
+
+    var elementHit = e.detail.body.el;
+
+    if (elementHit.id.includes("box")) {
+      elementHit.setAttribute("material", {
+        opacity: 0.6,
+        transparent: true,
+      });
+
+
+          // applyImpulse(impulse,worldPoint)
+        //impulse-It is the amount of impulse to add to the body
+        //worldPoint- It is the point at which the force is applied.
+        //new CANNON.Vec3()--->creating new object
+        //e CANNON.Vec3().copy()
+
+
+        var impulse = new CANNON.Vec3(-2,2,1)
+        var worldPoint = new CANNON.Vec3().copy(
+          elementHit.getAttribute("position")
+        )
+
+        elementHit.body.applyImpulse(impulse, worldPoint)
+
+      //remove event listener
+      element.removeEventListener("collide", this.shoot);
+
+      //remove the bullets from the scene
+      var scene = document.querySelector("#scene");
+      scene.removeChild(element);
+    }
   },
 });
