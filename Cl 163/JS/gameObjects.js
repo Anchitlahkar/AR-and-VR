44fef49b547571b71cb16e94090a38f5d959486d
Anchit Lahkar <anchitlahkar0202@gmail.com>
2021-12-11 12:07:03 +0530

    11.12.2021

--- Cl 163/JS/gameObjects.js
+++ Cl 163/JS/gameObjects.js
@@ -78,20 +78,32 @@ AFRAME.registerComponent("wire-fence", {
 
 AFRAME.registerComponent("boxes", {
   schema: {
-    height: { type: "number", default: 3 },
-    width: { type: "number", default: 3 },
-    depth: { type: "number", default: 3 },
+    height: { type: "number", default: 2 },
+    width: { type: "number", default: 2 },
+    depth: { type: "number", default: 2 },
   },
 
   init: function () {
     // Do something when component first attached.
+    //x position array
+    px = [
+      22.86, -17.35, -12.81, 0.44, -30.18, -25.89, 15.61, 29.68, 11.95, -15.4,
+      -14.09, 34.76, 2.29, 21.77, 1.57, 34.72, 12.04, -10.9, 6.48, 15.66,
+    ];
+
+    //z position array
+    pz = [
+      54.56, -4.71, 14.91, 56.74, 41.13, 50.76, 57.84, 7.02, -5.24, -26.82,
+      27.59, -35.78, 34.52, 31.32, -9.22, 26.72, 48.9, 27.24, 9.94, 54.29,
+    ];
+
     for (var i = 0; i < 20; i++) {
       var box = document.createElement("a-entity");
       box.setAttribute("id", "box" + i);
 
-      posX = Math.random() * 200 - 100;
+      posX = px[i];
       posY = 1.5;
-      posZ = Math.random() * 200 - 100;
+      posZ = pz[i];
 
       var position = { x: posX, y: posY, z: posZ };
       box.setAttribute("position", position);
