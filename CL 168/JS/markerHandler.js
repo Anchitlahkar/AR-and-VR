af67d439bbb3ee3da4e35e32ec53395466bed81a
Anchit Lahkar <anchitlahkar0202@gmail.com>
2022-01-20 12:35:25 +0530

    20.01.2022

--- CL 168/JS/markerHandler.js
+++ CL 168/JS/markerHandler.js
@@ -73,6 +73,7 @@ AFRAME.registerComponent("marker-handler", {
       var ratingButton = document.getElementById("rating-button");
       var orderButton = document.getElementById("order-button");
       var orderSummaryButton = document.getElementById("order-summary-button");
+      var payButton = document.getElementById("pay-button");
 
       //handling click events
       ratingButton.addEventListener("click", function () {
@@ -99,6 +100,10 @@ AFRAME.registerComponent("marker-handler", {
 
       orderSummaryButton.addEventListener("click", () => {
         this.handleOrderSummary();
+      });
+
+      payButton.addEventListener("click", () => {
+        this.handlePayment();
       });
     }
   },
@@ -175,11 +180,113 @@ AFRAME.registerComponent("marker-handler", {
   handleOrderSummary: async function () {
     //Getting Table Number
 
-    var tNumber ;
+    var tNumber;
     tableNumber <= 9 ? (tNumber = `T0${tableNumber}`) : `T${tableNumber}`;
 
     //Getting Order Summary from database
     var orderSummary = await this.getOrderSummary(tNumber);
+
+    //Changing modal div visibility
+    var modalDiv = document.getElementById("modal-div");
+    modalDiv.style.display = "flex";
+
+    var tableBodyTag = document.getElementById("bill-table-body");
+
+    //Removing the old tr data
+    tableBodyTag.innerHTML = "";
+
+    //Get the current orders key
+    var currentOrders = Object.keys(orderSummary.current_orders);
+
+    currentOrders.map((i) => {
+      //Create table row
+      var tr = document.createElement("tr");
+
+      //Create table cells/columns for ITEM NAME, PRICE, QUANTITY & TOTAL PRICE
+      var item = document.createElement("td");
+      var price = document.createElement("td");
+      var quantity = document.createElement("td");
+      var subtotal = document.createElement("td");
+
+      //Add HTML content
+      item.innerHTML = orderSummary.current_orders[i].item;
+
+      price.innerHTML = "$" + orderSummary.current_orders[i].price;
+      price.setAttribute("class", "text-center");
+
+      quantity.innerHTML = orderSummary.current_orders[i].quantity;
+      quantity.setAttribute("class", "text-center");
+
+      subtotal.innerHTML = "$" + orderSummary.current_orders[i].subtotal;
+      subtotal.setAttribute("class", "text-center");
+
+      //Append cells to the row
+      tr.appendChild(item);
+      tr.appendChild(price);
+      tr.appendChild(quantity);
+      tr.appendChild(subtotal);
+
+      //Append row to the table
+      tableBodyTag.appendChild(tr);
+    });
+
+    //Create a table row to Total bill
+    var totalTr = document.createElement("tr");
+
+    //Create a empty cell (for not data)
+    var td1 = document.createElement("td");
+    td1.setAttribute("class", "no-line");
+
+    var td2 = document.createElement("td");
+    td2.setAttribute("class", "no-line");
+
+    //Create a cell for TOTAL
+    var td3 = document.createElement("td");
+    td3.setAttribute("class", "no-line text-center");
+
+    //Create <strong> element to emphasize the text
+    var strongTag = document.createElement("strong");
+    strongTag.innerHTML = "Total";
+
+    td3.appendChild(strongTag);
+
+    //Create cell to show total bill amount
+    var td4 = document.createElement("td");
+    td1.setAttribute("class", "no-line text-right");
+    td4.innerHTML = "$" + orderSummary.total_bill;
+
+    //Append cells to the row
+    totalTr.appendChild(td1);
+    totalTr.appendChild(td2);
+    totalTr.appendChild(td3);
+    totalTr.appendChild(td4);
+
+    //Append the row to the table
+    tableBodyTag.appendChild(totalTr);
+  },
+
+  handlePayment: function () {
+    //Close modal
+    document.getElementById("modal-div").style.display = "none";
+
+    //Getting table number
+    var tNumber;
+    tableNumber <= 9 ? (tNumber = `T0${tableNumber}`) : `T${tableNumber}`;
+
+    //Resetting current orders and total bill
+    firebase.firestore().collection("tables").doc(tNumber).update({
+      current_Orders: {},
+      total_bill: 0,
+    })
+    .then(()=>{
+      swal({
+        icon: "success",
+        title: "Thanks For Paying !",
+        text: "We Hope You Enjoyed Your Food !!",
+        timer: 2500,
+        buttons: false
+      });
+    })
 
   },
 });
